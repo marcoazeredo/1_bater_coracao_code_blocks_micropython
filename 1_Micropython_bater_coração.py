e82084c837ec8e614453542dec9abcb3c1daf21e
Marco Azeredo <32407447+marcoazeredo@users.noreply.github.com>
2023-01-10 16:27:58 -0100

    Add files via upload

---
+++ 1_Micropython_bater_coração.py
@@ -0,0 +1,8 @@
+# Biblioteca Microbit
+from microbit import *
+# Loop infinito do conjunto de passos dados na execução da tarefa
+while True :
+    display.show(Image.HEART)
+    sleep(1000)
+    display.show(Image.HEART_SMALL)
+    sleep(1000)
